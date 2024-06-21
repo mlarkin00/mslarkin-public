a464f453bb951b33c84c6160f2ade6bbbe143e39
Matt Larkin <matthewlarkin@google.com>
2024-06-21 15:35:02 -0700

    Cleaning up publisher

--- go-pubsub-example/publisher/main.go
+++ go-pubsub-example/publisher/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
@@ -19,7 +18,8 @@ import (
 // /////////////////////////
 var topicId = "my-topic"
 var projectId = "my-project-id"
-var messagesPerSecond = 20 //Number of messages per second to publish
+var messagesPerSecond = 200 //Number of messages per second to publish
+var messagesPerBatch = 1    //Set to 1 to disable batch publishing
 ///////////////////////////
 
 // Create channel to listen for signals.
@@ -29,74 +29,46 @@ func main() {
 	// SIGINT handles Ctrl+C locally.
 	// SIGTERM handles Cloud Run termination signal.
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	ctx := context.Background()
 
-	http.HandleFunc("/", publishHandler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	go func() {
-		// Start HTTP server.
-		if err := http.ListenAndServe(":"+port, nil); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
+	//Get configured messagesPerSecond
 	mps := os.Getenv("MESSAGES_PER_SECOND")
 	if len(mps) > 0 {
 		messagesPerSecond, _ = strconv.Atoi(mps)
 	}
 
-	//Publish messages based on configured messagesPerSecond
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	for i := 0; i < messagesPerSecond; i++ {
-		go func() {
-			for {
-				msg := "Hello World (generated)"
-				err := publishMessage(ctx, projectId, topicId, msg)
-				if err != nil {
-					log.Fatal(err)
-
-				}
-				time.Sleep(1 * time.Second)
-				select {
-				case <-ctx.Done():
-					return
-				default:
-				}
-			}
-		}()
+	client, err := pubsub.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer client.Close()
+
+	msg := "Hello World (generated)"
+	topic := client.Topic(topicId)
+	topic.PublishSettings.CountThreshold = messagesPerBatch
+
+	go func() {
+		for {
+			//Publish messages based on configured messagesPerSecond
+			for b := 0; b < messagesPerSecond; b++ {
+				go publishMessage(ctx, topic, msg)
+			}
+			time.Sleep(1 * time.Second)
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+		}
+	}()
 
 	// Receive output from signalChan.
 	sig := <-signalChan
 	fmt.Printf("%s signal caught\n", sig)
 }
 
-func publishHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithCancel(r.Context())
-	defer cancel()
-	err := publishMessage(ctx, projectId, topicId, "Hello World (handler)")
-	if err != nil {
-		log.Fatal(err)
-	}
+func publishMessage(ctx context.Context, topic *pubsub.Topic, message string) {
 
-	fmt.Fprintf(w, "Message published")
-}
-
-func publishMessage(ctx context.Context, projectID, topicId string, message string) error {
-	client, err := pubsub.NewClient(ctx, projectID)
-	if err != nil {
-		return fmt.Errorf("pubsub.NewClient: %w", err)
-	}
-	defer client.Close()
-
-	topic := client.Topic(topicId)
 	result := topic.Publish(ctx, &pubsub.Message{
 		Data: []byte(message),
 	})
@@ -104,8 +76,7 @@ func publishMessage(ctx context.Context, projectID, topicId string, message stri
 	// ID is returned for the published message.
 	id, err := result.Get(ctx)
 	if err != nil {
-		return fmt.Errorf("Get: %w", err)
+		fmt.Println(fmt.Errorf("Get: %w", err))
 	}
 	fmt.Printf("Published message ID: %v\n", id)
-	return nil
 }
