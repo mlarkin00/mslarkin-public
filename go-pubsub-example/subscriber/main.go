55e77b39bc1a9575dc496c73fbf3659d4803de7e
Matt Larkin <matthewlarkin@google.com>
2024-07-29 12:35:53 -0700

    Subscriber example template

--- go-pubsub-example/subscriber/main.go
+++ go-pubsub-example/subscriber/main.go
@@ -15,8 +15,8 @@ import (
 // /////////////////////////
 // Configure environment //
 // /////////////////////////
-var subscriptionId = "pull-queue-testing" //"my-pull-subscription"
-var projectId = "mslarkin-ext" //"my-project-id"
+var subscriptionId = "my-pull-subscription"
+var projectId = "my-project-id"
 var processingDelayMs = 1000 //Delay to simulate message processing time
 var maxOutstanding = 1000   //Maximum number of concurrent messages
 ///////////////////////////
